7ba1ea1a0342e83a5ba8082c9ed16388448b461c
Caue Kepler <caue.kepler@dimy.lan>
2026-03-12 21:58:57 -0300

    Corrige roteamento para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ServiceDetail from "./pages/ServiceDetail";
 import ProductDetail from "./pages/ProductDetail";
@@ -17,17 +17,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <>
         <WhatsAppButton />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/servicos/:slug" element={<ServiceDetail />} />
           <Route path="/produtos/:slug" element={<ProductDetail />} />
           <Route path="/projetos/:slug" element={<ProjetoDetail />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </>
     </TooltipProvider>
   </QueryClientProvider>
 );
